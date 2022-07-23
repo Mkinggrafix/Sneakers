51fec3cc983f093593a9148128dc0c2ed13a5a0e
codeboyfriend <mkinggrafix@gmail.com>
2022-07-23 09:08:42 -0700

    initial

--- src/lib/Commerce.jsx
+++ src/lib/Commerce.jsx
@@ -1,3 +1,3 @@
 import Commerce from "@chec/commerce.js";
 
-export const commerce = new Commerce(import.meta.env.VITE_APP_CHEC_PUBLIC_KEY);
+export const commerce = new Commerce('pk_452243a5285d6527f3aa2453ba08fd77fde9cfc219e66');
